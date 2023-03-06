76960512565f08480cf160978321979e68721954
narongrit <narongrit.ku@psru.ac.th>
2023-03-06 14:18:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Mybar from "./components/Mybar.vue";
+import Mycard from "./components/Mycard.vue";
+// import items from "./attractions.json";
+</script>
+
+<template>
+  <header>
+    <Mybar />
+  </header>
+
+  <main>
+    <h1 style="padding-left: 10px">Attractions</h1>
+    <div class="grid-container">
+      <!-- <Mycard v-for="item in items" v-bind="item"/> -->
+      <!-- <Mycard v-for="todo in items" :key="todo" v-bind="todo" /> -->
+      <Mycard v-for="item in items" v-bind:key="item.id" v-bind="item" />
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    fetch('https://www.melivecode.com/api/attractions')
+    .then(res => res.json())
+    .then((result) => {
+      this.items = result
+    })
+  }
+}
+</script>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+}
+
+@media (min-width: 576px) {
+  .grid-container {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+@media (min-width: 992px) {
+  .grid-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+@media only screen and (min-width: 1200px) {
+  .grid-container {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+</style>
---
+++ src/components/Mycard.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <div>
+    <div class="card">
+      <img :src="coverimage" :alt="name" style="width: 100%" />
+      <div className="container">
+        <h3>
+          <b>{{ name }}</b>
+        </h3>
+        <p>
+          {{ detail }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["id", "name", "detail", "coverimage"],
+};
+</script>
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  padding-bottom: 10px;
+}
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
